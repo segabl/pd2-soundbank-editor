b01ab2a99e057bfeb61c2b4b08700a6ea7e1a97a
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-12-03 22:22:29 +0100

    Make SoundBank parsing more robust

--- SoundBank.cs
+++ SoundBank.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
 
-// see https://bobdoleowndu.github.io/mgsv/documentation/soundswapping.html
+// http://wiki.xentax.com/index.php/Wwise_SoundBank_(*.bnk)
 
 namespace PD2SoundBankEditor {
 	public class StreamDescription {
@@ -44,116 +45,86 @@ namespace PD2SoundBankEditor {
 
 		private string filePath;
 
-		private string headerString;
-		private uint headerChunkLen;
-		private byte[] headerData;
-
-		private string didxString;
-		private uint didxChunkLen;
+		private List<KeyValuePair<string, byte[]>> sectionData = new List<KeyValuePair<string, byte[]>>();
 
 		private List<StreamDescription> streamDescriptions;
-
-		private string dataString;
-		private uint dataChunkLen;
-
-		private List<KeyValuePair<string, byte[]>> otherData = new List<KeyValuePair<string, byte[]>>();
 
 		public SoundBank(string file) {
 			filePath = file;
 
 			using var reader = new BinaryReader(new FileStream(file, FileMode.Open));
 
-			// BKHD header
-			headerString = Encoding.UTF8.GetString(reader.ReadBytes(4));
-			headerChunkLen = reader.ReadUInt32();
-			Trace.WriteLine($"{headerString} {headerChunkLen}");
-			// BKHD data
-			headerData = reader.ReadBytes((int)headerChunkLen);
-
-			// DIDX header
-			didxString = Encoding.UTF8.GetString(reader.ReadBytes(4));
-			if (didxString != "DIDX") {
-				throw new FileFormatException($"Unsupported soundbank type with header \"{didxString}\".");
+			// Read all headers and their data
+			while (true) {
+				var sectionString = Encoding.UTF8.GetString(reader.ReadBytes(4));
+				if (sectionString == "") {
+					break;
+				}
+				var sectionLength = reader.ReadUInt32();
+				Trace.WriteLine($"{sectionString} {sectionLength}");
+				sectionData.Add(new KeyValuePair<string, byte[]>(sectionString, reader.ReadBytes((int)sectionLength)));
 			}
-			didxChunkLen = reader.ReadUInt32();
-			Trace.WriteLine($"{didxString} {didxChunkLen}");
-			// DIDX data
+
+			var didxIndex = sectionData.FindIndex(x => x.Key == "DIDX");
+			var dataIndex = sectionData.FindIndex(x => x.Key == "DATA");
+
+			if (didxIndex < 0 || dataIndex < 0) {
+				throw new FileFormatException($"Unsupported soundbank type, missing DIDX/DATA headers.");
+			}
+
+			// Get the DIDX data
+			var didxData = sectionData[didxIndex].Value;
+			using var didxReader = new BinaryReader(new MemoryStream(didxData));
 			streamDescriptions = new List<StreamDescription>();
-			for (var i = 0; i < didxChunkLen; i += 12) {
+			for (var i = 0; i < didxData.Length; i += 12) {
 				var desc = new StreamDescription {
-					id = reader.ReadUInt32(),
-					dataOffset = reader.ReadUInt32(),
-					dataLength = reader.ReadUInt32()
+					id = didxReader.ReadUInt32(),
+					dataOffset = didxReader.ReadUInt32(),
+					dataLength = didxReader.ReadUInt32()
 				};
 				streamDescriptions.Add(desc);
 			}
 
-			// DATA header
-			dataString = Encoding.UTF8.GetString(reader.ReadBytes(4));
-			dataChunkLen = reader.ReadUInt32();
-			Trace.WriteLine($"{dataString} {dataChunkLen}");
-			// DATA data
+			// Get the DATA data
+			var dataData = sectionData[dataIndex].Value;
+			using var dataReader = new BinaryReader(new MemoryStream(dataData));
 			for (var i = 0; i < streamDescriptions.Count; i++) {
 				var desc = streamDescriptions[i];
 				Trace.WriteLine($"Reading data for {desc.id}...");
-				desc.data = reader.ReadBytes((int)desc.dataLength);
-				var nextOffset = i < streamDescriptions.Count - 1 ? streamDescriptions[i + 1].dataOffset : dataChunkLen;
+				desc.data = dataReader.ReadBytes((int)desc.dataLength);
+				var nextOffset = i < streamDescriptions.Count - 1 ? streamDescriptions[i + 1].dataOffset : (uint)dataData.Length;
 				var paddingAmount = nextOffset - desc.dataOffset - desc.dataLength;
-				reader.ReadBytes((int)paddingAmount);
-			}
-
-			// All other remaining data
-			while (true) {
-				var otherString = Encoding.UTF8.GetString(reader.ReadBytes(4));
-				if (otherString == "") {
-					return;
-				}
-				var otherChunkLen = reader.ReadUInt32();
-				Trace.WriteLine($"{otherString} {otherChunkLen}");
-				otherData.Add(new KeyValuePair<string, byte[]>(otherString, reader.ReadBytes((int)otherChunkLen)));
+				dataReader.ReadBytes((int)paddingAmount);
 			}
 		}
 
 		public void Save(string file) {
 
-			using var writer = new BinaryWriter(new FileStream(file, FileMode.Create));
-
-			// BKHD data
-			writer.Write(Encoding.UTF8.GetBytes(headerString));
-			writer.Write(headerChunkLen);
-			writer.Write(headerData);
-
-			// DIDX data
-			writer.Write(Encoding.UTF8.GetBytes(didxString));
-			writer.Write(streamDescriptions.Count * 12); // listChunkLen
+			// Write new DIDX and DATA
+			using var didxWriter = new BinaryWriter(new MemoryStream());
+			using var dataWriter = new BinaryWriter(new MemoryStream());
 			var totalDataSize = 0;
 			foreach (var desc in streamDescriptions) {
 				var align = 16 - (totalDataSize % 16); // pad to nearest 16
 				if (align < 16) {
+					dataWriter.Write(new byte[align]);
 					totalDataSize += align;
 				}
-				writer.Write(desc.id);
-				writer.Write(totalDataSize); // wem.dataOffset
-				writer.Write(desc.data.Length); // wem.dataLength
+
+				didxWriter.Write(desc.id);
+				didxWriter.Write(totalDataSize);
+				didxWriter.Write(desc.data.Length);
+
+				dataWriter.Write(desc.data);
+
 				totalDataSize += desc.data.Length;
 			}
+			sectionData[sectionData.FindIndex(x => x.Key == "DIDX")] = new KeyValuePair<string, byte[]>("DIDX", ((MemoryStream)didxWriter.BaseStream).ToArray());
+			sectionData[sectionData.FindIndex(x => x.Key == "DATA")] = new KeyValuePair<string, byte[]>("DATA", ((MemoryStream)dataWriter.BaseStream).ToArray());
 
-			// DATA data
-			writer.Write(Encoding.UTF8.GetBytes(dataString));
-			writer.Write(totalDataSize); // dataChunkLen
-			var bytesWritten = 0;
-			foreach (var desc in streamDescriptions) {
-				var align = 16 - (bytesWritten % 16);  // pad to nearest 16
-				if (align < 16) {
-					writer.Write(new byte[align]);
-					bytesWritten += align;
-				}
-				writer.Write(desc.data);
-				bytesWritten += desc.data.Length;
-			}
-
-			// All other remaining data
-			foreach (var pair in otherData) {
+			// Write all data to file
+			using var writer = new BinaryWriter(new FileStream(file, FileMode.Create));
+			foreach (var pair in sectionData) {
 				writer.Write(Encoding.UTF8.GetBytes(pair.Key));
 				writer.Write(pair.Value.Length);
 				writer.Write(pair.Value);
