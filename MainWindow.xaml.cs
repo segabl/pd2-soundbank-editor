d296c80ecccef4ddabd23e6d059d4f1f9c81240c
Sebastian Gabl <sebastian.gabl@gmx.at>
2020-12-03 15:23:25 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,221 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace PD2SoundBankEditor {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : AdonisUI.Controls.AdonisWindow {
+		static readonly string CONVERTER_NAME = "wwise_ima_adpcm.exe";
+		static readonly string CONVERTER_PATH = System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, CONVERTER_NAME);
+		static readonly string TEMP_DIR = System.IO.Path.Join(AppDomain.CurrentDomain.BaseDirectory, "tmp");
+
+		private SoundBank soundBank;
+		private int extractErrors;
+		private Button playingButton;
+		private MediaPlayer mediaPlayer = new MediaPlayer();
+
+		public MainWindow() {
+			InitializeComponent();
+			if (!File.Exists(CONVERTER_PATH)) {
+				AdonisUI.Controls.MessageBox.Show($"The sound converter could not be found! Please place {CONVERTER_NAME} in the directory of this application!", "Information", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Warning);
+			}
+			mediaPlayer.MediaEnded += ChangePlayButton;
+			if (!Directory.Exists(TEMP_DIR)) {
+				Directory.CreateDirectory(TEMP_DIR);
+			}
+		}
+
+		private void OnImportButtonClick(object sender, RoutedEventArgs e) {
+			var diag = new OpenFileDialog {
+				Filter = "Soundbanks (*.bnk)|*.bnk"
+			};
+			if (diag.ShowDialog() != true) {
+				return;
+			}
+			importTextBox.Text = diag.FileName;
+			soundBank = new SoundBank(diag.FileName);
+			listView.ItemsSource = soundBank.GetWemFiles();
+			listView.DataContext = listView.ItemsSource;
+			extractAllButton.IsEnabled = true;
+			saveButton.IsEnabled = true;
+		}
+
+		private void OnListViewSelectionChanged(object sender, SelectionChangedEventArgs e) {
+			replaceSelectedButton.IsEnabled = listView.SelectedItems.Count > 0;
+			extractSelectedButton.IsEnabled = listView.SelectedItems.Count > 0;
+		}
+
+		private string StartConverterProcess(string args) {
+			if (!File.Exists(CONVERTER_PATH)) {
+				return $"The sound converter could not be found! Please place {CONVERTER_NAME} in the directory of this application!";
+			}
+			Trace.WriteLine($"Running converter with arguments {args}");
+			var convertProcess = new Process();
+			convertProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			convertProcess.StartInfo.UseShellExecute = false;
+			convertProcess.StartInfo.RedirectStandardOutput = true;
+			convertProcess.StartInfo.FileName = CONVERTER_PATH;
+			convertProcess.StartInfo.Arguments = args;
+			convertProcess.Start();
+			var output = convertProcess.StandardOutput.ReadToEnd();
+			convertProcess.WaitForExit();
+			return output;
+		}
+
+		private void ExtractWemFiles(IEnumerable<WemFile> wemFiles) {
+			mainGrid.IsEnabled = false;
+			BackgroundWorker worker = new BackgroundWorker {
+				WorkerReportsProgress = true
+			};
+			worker.DoWork += ExtractWemFilesAsync;
+			worker.ProgressChanged += OnExtractWemFilesProgress;
+			worker.RunWorkerCompleted += OnExtractWemFilesCompleted;
+			worker.RunWorkerAsync(wemFiles);
+		}
+
+		private void ExtractWemFilesAsync(object sender, DoWorkEventArgs e) {
+			var wemFiles = (IEnumerable<WemFile>)e.Argument;
+			var soundBankName = soundBank.GetFilePath();
+			var savePath = System.IO.Path.Join(System.IO.Path.GetDirectoryName(soundBankName), System.IO.Path.GetFileNameWithoutExtension(soundBankName));
+			if (!Directory.Exists(savePath)) {
+				Directory.CreateDirectory(savePath);
+			}
+			var converterAvailable = File.Exists(CONVERTER_PATH);
+			var n = 0;
+			extractErrors = 0;
+			foreach (var wem in wemFiles) {
+				var errorStr = "";
+				var fileName = System.IO.Path.Join(savePath, wem.id.ToString() + ".stream");
+				var convertedFileName = System.IO.Path.ChangeExtension(fileName, "wav");
+				if (!wem.Save(fileName)) {
+					errorStr = wem.errorString;
+				} else if (converterAvailable) {
+					errorStr = StartConverterProcess($"-d \"{fileName}\" \"{convertedFileName}\"");
+				}
+				if (errorStr != "") {
+					errorStr = $"Something went wrong while processing {wem.id}:\n{errorStr}";
+					extractErrors++;
+				} else {
+					wem.convertedFilePath = convertedFileName;
+				}
+				(sender as BackgroundWorker).ReportProgress((int)(++n / (float)wemFiles.Count() * 100), errorStr);
+			}
+		}
+
+		void OnExtractWemFilesProgress(object sender, ProgressChangedEventArgs e) {
+			progressBar.Value = e.ProgressPercentage;
+			var errorString = (string)e.UserState;
+			if (errorString != null && errorString != "") {
+				AdonisUI.Controls.MessageBox.Show(errorString, "Error", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Error);
+			}
+		}
+
+		void OnExtractWemFilesCompleted(object sender, RunWorkerCompletedEventArgs e) {
+			if (extractErrors > 0) {
+				AdonisUI.Controls.MessageBox.Show($"Extraction completed with {extractErrors} error(s)!", "Information", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Warning);
+			} else {
+				AdonisUI.Controls.MessageBox.Show("Extraction complete!", "Information", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Information);
+			}
+			progressBar.Value = 0;
+			mainGrid.IsEnabled = true;
+		}
+
+		private void OnExtractButtonClick(object sender, RoutedEventArgs e) {
+			ExtractWemFiles(((Button)sender) == extractAllButton ? soundBank.GetWemFiles() : listView.SelectedItems.Cast<WemFile>());
+		}
+
+		private void OnSaveButtonClick(object sender, RoutedEventArgs e) {
+			var diag = new SaveFileDialog {
+				Filter = "Soundbanks (*.bnk)|*.bnk",
+				AddExtension = true
+			};
+			if (diag.ShowDialog() != true) {
+				return;
+			}
+			soundBank.Save(diag.FileName);
+			AdonisUI.Controls.MessageBox.Show("Soundbank saved!", "Information", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Information);
+		}
+
+		private void OnReplaceButtonClick(object sender, RoutedEventArgs e) {
+			var diag = new OpenFileDialog {
+				Filter = "Wave audio files (*.wav)|*.wav"
+			};
+			if (diag.ShowDialog() != true) {
+				return;
+			}
+			var fileNameNoExt = System.IO.Path.GetFileNameWithoutExtension(diag.FileName);
+			var fileName = System.IO.Path.Combine(TEMP_DIR, fileNameNoExt + ".stream");
+			var errorString = StartConverterProcess($"-e \"{diag.FileName}\" \"{fileName}\"");
+			if (errorString != "") {
+				AdonisUI.Controls.MessageBox.Show($"An error occured while trying to convert {diag.FileName}:\n{errorString}", "Error", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Error);
+				return;
+			}
+			var data = File.ReadAllBytes(fileName);
+			foreach (var wem in listView.SelectedItems.Cast<WemFile>()) {
+				wem.data = data;
+				wem.replacementFile = fileNameNoExt + ".wav";
+			}
+			listView.Items.Refresh();
+			AdonisUI.Controls.MessageBox.Show($"Files replaced!", "Information", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Information);
+		}
+
+		private void OnPlayButtonClick(object sender, RoutedEventArgs e) {
+			var button = (Button)sender;
+			var wem = (WemFile)button.DataContext;
+
+			Trace.WriteLine($"{wem.id} clicked");
+
+			var sameButton = playingButton == button;
+
+			mediaPlayer.Stop();
+			ChangePlayButton(sender, e);
+
+			if (sameButton) {
+				return;
+			}
+
+			if (wem.convertedFilePath == null) {
+				var fileName = System.IO.Path.Combine(TEMP_DIR, $"{wem.id}.stream");
+				var convertedFileName = System.IO.Path.ChangeExtension(fileName, "wav");
+				string errorString;
+				if (!wem.Save(fileName)) {
+					errorString = wem.errorString;
+				} else {
+					errorString = StartConverterProcess($"-d \"{fileName}\" \"{convertedFileName}\"");
+				}
+				File.Delete(fileName);
+				if (errorString != "") {
+					AdonisUI.Controls.MessageBox.Show($"Can't play file:\n{errorString}", "Error", AdonisUI.Controls.MessageBoxButton.OK, AdonisUI.Controls.MessageBoxImage.Error);
+					return;
+				}
+				wem.convertedFilePath = convertedFileName;
+			}
+			mediaPlayer.Open(new Uri(wem.convertedFilePath));
+			mediaPlayer.Play();
+			button.Content = "■";
+			playingButton = button;
+		}
+
+		private void ChangePlayButton(object sender, EventArgs e) {
+			if (playingButton != null) {
+				playingButton.Content = "▶";
+			}
+			playingButton = null;
+		}
+
+		private void OnWindowClosed(object sender, EventArgs e) {
+			if (Directory.Exists(TEMP_DIR)) {
+				Directory.Delete(TEMP_DIR, true);
+			}
+		}
+	}
+}
---
+++ SoundBank.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace PD2SoundBankEditor {
+	public class WemFile {
+		public uint id;
+		public uint dataOffset;
+		public byte[] data;
+		public byte[] unknownData;
+
+		public string errorString;
+		public string replacementFile;
+		public string convertedFilePath;
+
+		public string Name { get => id.ToString(); }
+		public string ReplacementFile { get => replacementFile; }
+
+		public override string ToString() {
+			return id.ToString();
+		}
+
+		public bool Save(string file) {
+			Trace.WriteLine($"Saving file {file}...");
+
+			try {
+				var writer = new BinaryWriter(new FileStream(file, FileMode.Create));
+				writer.Write(data);
+				writer.Close();
+			} catch (Exception e) {
+				errorString = e.Message;
+				return false;
+			}
+
+			return true;
+		}
+	}
+
+	public class SoundBank {
+
+		private string filePath;
+
+		private uint headerMagicNum;
+		private uint headerChunkLen;
+		private byte[] headerData;
+
+		private uint didxMagicNum;
+		private uint didxChunkLen;
+
+		private List<WemFile> wemFiles;
+
+		private uint dataMagicNum;
+		private uint dataChunkLen;
+
+		private byte[] unknownData;
+
+		public SoundBank(string file) {
+			filePath = file;
+
+			using var reader = new BinaryReader(new FileStream(file, FileMode.Open));
+
+			headerMagicNum = reader.ReadUInt32();
+			headerChunkLen = reader.ReadUInt32();
+			Trace.WriteLine($"Header {headerMagicNum} {headerChunkLen}");
+			headerData = reader.ReadBytes((int)headerChunkLen);
+
+			didxMagicNum = reader.ReadUInt32();
+			didxChunkLen = reader.ReadUInt32();
+			Trace.WriteLine($"DIDX {didxMagicNum} {didxChunkLen}");
+			wemFiles = new List<WemFile>();
+			for (var i = 0; i < didxChunkLen / 12; i++) {
+				var wem = new WemFile {
+					id = reader.ReadUInt32(),
+					dataOffset = reader.ReadUInt32(),
+					data = new byte[reader.ReadUInt32()]
+				};
+				wemFiles.Add(wem);
+			}
+
+			dataMagicNum = reader.ReadUInt32();
+			dataChunkLen = reader.ReadUInt32();
+			Trace.WriteLine($"Data {dataMagicNum} {dataChunkLen}");
+			for (var i = 0; i < wemFiles.Count; i++) {
+				var wem = wemFiles[i];
+				Trace.WriteLine($"Reading data for {wem.id}...");
+				wem.data = reader.ReadBytes(wem.data.Length);
+				var nextOffset = i < wemFiles.Count - 1 ? wemFiles[i + 1].dataOffset : dataChunkLen;
+				var unknownDataAmount = nextOffset - wem.dataOffset - wem.data.Length;
+				wem.unknownData = reader.ReadBytes((int)unknownDataAmount);
+			}
+
+
+			var num = 0;
+			var buffer = new byte[128];
+			var data = new List<byte>();
+			while ((num = reader.Read(buffer, 0, buffer.Length)) > 0) {
+				if (num == buffer.Length) {
+					data.AddRange(buffer);
+				} else {
+					for (var i = 0; i < num; i++) {
+						data.Add(buffer[i]);
+					}
+				}
+			}
+			unknownData = data.ToArray();
+			Trace.WriteLine($"{unknownData.Length} bytes of unknown data left");
+		}
+
+		public void Save(string file) {
+
+			using var writer = new BinaryWriter(new FileStream(file, FileMode.Create));
+
+			writer.Write(headerMagicNum);
+			writer.Write(headerChunkLen);
+			writer.Write(headerData);
+
+			writer.Write(didxMagicNum);
+			writer.Write(wemFiles.Count * 12); // didxChunkLen
+			var totalDataSize = 0;
+			foreach (var wem in wemFiles) {
+				writer.Write(wem.id);
+				writer.Write(totalDataSize);
+				writer.Write(wem.data.Length);
+				totalDataSize += wem.data.Length + wem.unknownData.Length;
+			}
+
+			writer.Write(dataMagicNum);
+			writer.Write(totalDataSize); // dataChunkLen
+			foreach (var wem in wemFiles) {
+				writer.Write(wem.data);
+				writer.Write(wem.unknownData);
+			}
+
+			writer.Write(unknownData);
+		}
+
+		public string GetFilePath() {
+			return filePath;
+		}
+
+		public List<WemFile> GetWemFiles() {
+			return wemFiles;
+		}
+	}
+}
